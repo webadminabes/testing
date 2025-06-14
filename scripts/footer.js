b8ad1a9c1271fe652282f45552ed3e00ef96f429
webadminabes <webadmin@abes.ac.in>
2025-06-14 12:18:53 +0530

    change in footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -7,26 +7,26 @@ footerHTML = `
           <h4>Mandatory Disclosures</h4>
           <div class="sub_cols">
             <div class="sub_col1">
-              <a target="_blank" href="./pdf/Footer_AICTE/Final_Mandatory_Disclosure.pdf">Mandatory Disclosure</a>
-              <a href="/footer-aicteapprovals.html">AICTE Approval</a>
-              <a href="/footer-affiliationletter.html">University Affiliation Letter</a>
-              <a target="_blank" href="./education-verification.html">Education Verification</a>
-              <a href="/auditedstatement.html">Audited Statement</a>
-              <a href="/footer-form.html">Student's Request Form (SIS)</a>
-              <a href="/Academic-Calender.html">Academic Calendar</a>
-              <a target="_blank" href="pdf/Balance Sheet FY 2022-23 ABES EC.pdf">Balance Sheet 2022-23</a>
-              <a target="_blank" href="pdf/Balance Sheet FY 2023-24 ABES EC.pdf">Balance Sheet 2023-24</a>
-              <a target="_blank" href="pdf/brochure.pdf">ABESEC Brochure 2021-22</a>
+              <a target="_blank" href="../pdf/Footer_AICTE/Final_Mandatory_Disclosure.pdf">Mandatory Disclosure</a>
+              <a href="../footer-aicteapprovals.html">AICTE Approval</a>
+              <a href="../footer-affiliationletter.html">University Affiliation Letter</a>
+              <a target="_blank" href="../education-verification.html">Education Verification</a>
+              <a href="../auditedstatement.html">Audited Statement</a>
+              <a href="../footer-form.html">Student's Request Form (SIS)</a>
+              <a href="../Academic-Calender.html">Academic Calendar</a>
+              <a target="_blank" href="../pdf/Balance Sheet FY 2022-23 ABES EC.pdf">Balance Sheet 2022-23</a>
+              <a target="_blank" href="../pdf/Balance Sheet FY 2023-24 ABES EC.pdf">Balance Sheet 2023-24</a>
+              <a target="_blank" href="../pdf/brochure.pdf">ABESEC Brochure 2021-22</a>
             </div>
             <div class="sub_col2">
-              <a target="_blank" href="pdf/HOSTEL RULE BOOKLET 2022-23 (4).pdf">Hostel Rule Booklet</a>
-              <a target="_blank" href="pdf/REFUNDPOLICYFORNEWLYADMITTEDSTUDENTSSESSION2024-25.pdf">Refund Policy</a>
-              <a href="/policycollege.html">College Policy</a>
-              <a href="/GRC-grievance-redressal-cell.html">Grievance Redressal Cell</a>
-              <a href="/footer-Committees.html">Committees</a>
-              <a target="_blank" href="pdf/ARIIA-2020-Report.-ARI-C-46112.pdf">Report ARIIA 2020</a>
-              <a target="_blank" href="pdf/TEDxABES/TEDxABESEC'24.pdf">TEDxABESEC'24 Brochure</a>
-              <a href="/circulars.html">Circulars/Notices</a>
+              <a target="_blank" href="../pdf/HOSTEL RULE BOOKLET 2022-23 (4).pdf">Hostel Rule Booklet</a>
+              <a target="_blank" href="../pdf/REFUNDPOLICYFORNEWLYADMITTEDSTUDENTSSESSION2024-25.pdf">Refund Policy</a>
+              <a href="../policycollege.html">College Policy</a>
+              <a href="../GRC-grievance-redressal-cell.html">Grievance Redressal Cell</a>
+              <a href="../footer-Committees.html">Committees</a>
+              <a target="_blank" href="../pdf/ARIIA-2020-Report.-ARI-C-46112.pdf">Report ARIIA 2020</a>
+              <a target="_blank" href="../pdf/TEDxABES/TEDxABESEC'24.pdf">TEDxABESEC'24 Brochure</a>
+              <a href="../circulars.html">Circulars/Notices</a>
             </div>
           </div>
         </div>
@@ -55,19 +55,19 @@ footerHTML = `
               <a href="https://publication.abes.ac.in/">Repository</a>
               <a href="https://nodues.abes.ac.in/">No-Dues Portal</a>
               <!--<a href="https://vqijstm.abes.ac.in/">Vision & Quest</a>-->
-              <a href="pdf/disclamer.pdf" target="_blank">Disclaimer</a>
-              <a href="/uav-journey.html">UAV</a>
-              <a href="/academics/IQAC-department.html">IQAC</a>
+              <a href="../pdf/disclamer.pdf" target="_blank">Disclaimer</a>
+              <a href="../uav-journey.html">UAV</a>
+              <a href="../academics/IQAC-department.html">IQAC</a>
             </div>
             <div class="sub_col2">
-              <a href="/courses-offered.html">Admissions</a>
-              <a href="/CAREER-department.html">Career</a>
-              <a href="/cetl-about.html">CETL (SIS)</a>
+              <a href="../courses-offered.html">Admissions</a>
+              <a href="../CAREER-department.html">Career</a>
+              <a href="../cetl-about.html">CETL (SIS)</a>
               <a href="https://tour.abes.ac.in/">Virtual Tour</a>
               <!--<a href="https://abesec.greythr.com/">HR portal</a>
-              <a href="/hr-policies.html">HR Policies</a>-->
-              <a href="/iandepolicies.html">I & E Policies</a>
-              <a href="pdf/library/About Library.pdf">Library</a>
+              <a href="../hr-policies.html">HR Policies</a>-->
+              <a href="../iandepolicies.html">I & E Policies</a>
+              <a href="../pdf/library/About Library.pdf">Library</a>
               <a href="https://forms.office.com/r/25QfSYdDdg" target="/blank">Placement Referal Link</a>
 
             </div>
